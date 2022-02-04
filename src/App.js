61505a2f5c7e124c0420f293a2189ffd678cc50e
vegisama123 <thota.avinash22@gmail.com>
2022-02-04 11:00:26 +0530

    intialcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import react,{useState} from 'react';
 
 function App() {
+  const [input,setInput] = useState("");
+  const [result,setResult] = useState("");
+const changeHandler =(e)=>{
+  setInput(e.target.value)
+}
+const submitHandler =(e)=>{
+  setResult(eval(input))
+  
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="text" name='text' value={input} onChange={changeHandler}/><br/>
+    <button onClick={submitHandler}>result</button>
+    <h1>{result}</h1>
+     <button onClick={()=>setInput(input+'1')}>1</button>
+     <button onClick={()=>setInput(input+'2')}>2</button>
+     <button onClick={()=>setInput(input+'3')}>3</button><br/>
+     <button onClick={()=>setInput(input+'4')}>4</button>
+     <button onClick={()=>setInput(input+'5')}>5</button>
+     <button onClick={()=>setInput(input+'6')}>6</button><br/>
+     <button onClick={()=>setInput(input+'7')}>7</button>
+     <button onClick={()=>setInput(input+'8')}>8</button>
+     <button onClick={()=>setInput(input+'9')}>9</button><br/>
+     <button onClick={()=>setInput(input+'0')}>0</button>
+
+     <button onClick={()=>setInput(input+'+')}>+</button>
+     <button onClick={()=>setInput(input+'-')}>-</button>
+     <button onClick={()=>setInput(input+'*')}>*</button>
+     <button onClick={()=>setInput(input+'%')}>%</button>
+     <button onClick={()=>setInput('')}>clr</button>
+
+      
     </div>
   );
 }
